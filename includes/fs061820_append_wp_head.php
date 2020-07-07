2e2cd96c24915eba5250c78dfe69dc3377751f2d
FreshyMichael <michael@freshysites.com>
2020-07-07 12:23:05 -0400

    Update fs061820_append_wp_head.php

--- includes/fs061820_append_wp_head.php
+++ includes/fs061820_append_wp_head.php
@@ -15,7 +15,7 @@ else{}
 ?>
 <!-- DataTables via CDN implementation -->
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.18/b-1.5.6/r-2.2.2/rg-1.1.0/rr-1.2.4/sc-2.0.0/sl-1.3.0/datatables.min.css"/> 
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.18/b-1.5.6/r-2.2.2/rg-1.1.0/rr-1.2.4/sc-2.0.0/sl-1.3.0/datatables.min.css"/>
 <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.18/b-1.5.6/r-2.2.2/rg-1.1.0/rr-1.2.4/sc-2.0.0/sl-1.3.0/datatables.min.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/select/1.3.0/js/dataTables.select.min.js"></script>
@@ -29,9 +29,9 @@ jQuery(document).ready(function ( $ ) {
 
 $(document).ready(function() {
 	var table = $('.tablepress').DataTable( {
-		
-	"searching": true,
-		
+
+			"searching": true,
+			sDom: '<"search-box"r>lftip',
 			dom: 'trp',
 			columnDefs: [ {
 					orderable: false,
@@ -112,10 +112,10 @@ jQuery.noConflict();
 jQuery(document).ready(function ( $ ) {
 	columnTh = $("table th:contains('Insertions')");
 	columnIndex = columnTh.index() +9;
-	$('table tr td:nth-child(' + columnIndex + ')').css("whitespace", "nowrap"); 
+	$('table tr td:nth-child(' + columnIndex + ')').css("whitespace", "nowrap");
 	 columnTh.css("whitespace", "nowrap");
 });
-	
+
 //Expanding td content
 
 //Column 2
@@ -181,18 +181,18 @@ jQuery(document).ready(function ( $ ) {
 .tablepress .sorting:hover, .tablepress .sorting_asc, .tablepress .sorting_desc {
     background-color: #d9edf7;
 }
-	
+
 	.hidden{
 		display:none;
 	}
 
-	i.material-icons{	
+	i.material-icons{
  font-size:24px;
-	color:#003990; 
+	color:#003990;
 	cursor:pointer;
 	float:right;
 	}
-	
+
 	.paginate_button  {
 	background-color:#003990;
     color: #FFFFFF;
